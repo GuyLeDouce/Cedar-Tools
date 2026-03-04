6561a04510ed2a3778b6e2eaea80184493647da5
GuyLeDouce <guyledouce69@gmail.com>
2026-03-04 16:17:41 -0500

    Update PWA icon and app name to CW Tools

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cedar-tools-v2";
+const CACHE_NAME = "cedar-tools-v3";
 const APP_SHELL = [
   "/",
   "/index.html",
@@ -14,7 +14,7 @@ const APP_SHELL = [
   "/styles.css",
   "/app.js",
   "/manifest.webmanifest",
-  "/icons/icon.svg"
+  "/icons/ToolsLogo.jpg"
 ];
 
 self.addEventListener("install", (event) => {
